08c212aef0e9a664c2b53df7c1e9ae0536e32aad
Netanel Ozeri <NetanelO@psagot.co.il>
2021-10-06 14:56:05 +0300

    reattach

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -38,7 +38,7 @@ export class ListComponent implements OnInit, OnDestroy {
       this.page = data;
     });
     this.ref.detach();
-    setInterval(() => { this.ref.detectChanges(); }, 100);
+    setTimeout(() => { this.ref.reattach();}, 50);
   }
 
   displayFunc() {
