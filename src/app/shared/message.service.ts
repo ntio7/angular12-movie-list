1aeb9b711e44822cc435f07d0223bbe729d37efe
Netanel Ozeri <NetanelO@psagot.co.il>
2021-10-06 07:48:54 +0300

    use Subject in MessageService

--- src/app/shared/message.service.ts
+++ src/app/shared/message.service.ts
@@ -1,21 +1,43 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit, OnDestroy } from '@angular/core';
 import { Movie } from './models/movie';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MessageService {
- 
-  constructor() { }
+export class MessageService implements OnInit, OnDestroy {
+
+  subject: Subject<Movie[]> = new Subject<Movie[]>()
+
   movie: Movie = new Movie();
   movies: Movie[] = [];
 
-  sendMovies(movies: Movie[]) {
-    this.movies = movies;
+  constructor() { }
+
+  ngOnInit() {
+  
   }
 
-  receiveMovie(movieId: number): Movie { 
-    return  this.movies[movieId];
+  sendMovies(movies: Movie[]) {  
+
+    this.subject.subscribe(data => {
+      this.movies = data;
+    });
+    
+    this.subject.next(movies);
+  }
+
+  receiveMovie(movieId: number): Movie {
+    return this.movies[movieId];
+  }
+
+  receiveMovies(): Movie[] {
+    return this.movies;
+  }
+
+  ngOnDestroy() {
+
+    this.subject.unsubscribe();
   }
 
 
